47bbe1933379d5d6b428a5eb7b1b410f3920f610
NASAMEERKHAN <sameerkhansk8019593@gmail.com>
2026-04-07 01:57:19 +0530

    fixed hod entity

--- src/main/java/com/sameer/exampay/entity/Hod.java
+++ src/main/java/com/sameer/exampay/entity/Hod.java
@@ -1,40 +1,51 @@
 package com.sameer.exampay.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Hod {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String username;
-
     private String password;
-    private String department;
 
-    // Getters and Setters
+    @Column(name = "department")   // 🔥 FIX
+    private String dept;
+
+    // getters
+    public Long getId() {
+        return id;
+    }
 
     public String getUsername() {
         return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getPassword() {
         return password;
     }
 
+    public String getDept() {
+        return dept;
+    }
+
+    // setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
+    public void setDept(String dept) {
+        this.dept = dept;
     }
 }
